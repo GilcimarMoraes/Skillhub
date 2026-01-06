b1fd577c6a8acc94db2c68e14f12992fc52bb9a7
GilcimarMoraes <myrraws@yahoo.com.br>
2026-01-06 01:39:14 -0300

    Adicionando novas classes

--- src/main/java/com/gil/skillhubapi/model/ServicoOferecidos.java
+++ src/main/java/com/gil/skillhubapi/model/ServicoOferecidos.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jdk.jfr.Timestamp;
 import lombok.AllArgsConstructor;
@@ -11,21 +13,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table( name = "servicos", schema = "skillhub" )
+@Table( name = "servicos_oferecidos", schema = "skillhub" )
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Servico {
+public class ServicoOferecidos {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
 
-    private String nome;
+    @ManyToOne
+    @JoinColumn( name = "servico_id" )
+    private Servico servico;
+
     private String descricao;
+
+    private String nome;
+
+    private BigDecimal valor;
+
+    @ManyToOne
+    @JoinColumn( name = "usuario_id" )
+    private Usuario usuario;
+
+    private String descricao;
+
     private Boolean ativo;
 
     @Timestamp
